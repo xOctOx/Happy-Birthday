98b6da87150219e238746628368119899e75d914
Ouattara Cheick <cheickiouattata@gmail.com>
2026-07-08 16:27:29 +0000

    Ajustement de l'image androidparty

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -72,7 +73,8 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
     Box {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
         )
         GreetingText(
             message = message,
